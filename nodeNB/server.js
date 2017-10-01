f123cad436f604978c11972045ae12a787753b4b
9prashant-1223 <prashantverma1223@gmail.com>
2017-10-01 07:39:07 +0530

    added server error and link to html file for server usage

--- nodeNB/server.js
+++ nodeNB/server.js
@@ -1,12 +1,26 @@
 var http = require('http');
+var fs   = require('fs');
 var port = 2018;
 
-function onRequest(req,res){
-    res.writeHead(200,{'Context-Type':"text/plain"});
-    res.write("here is my text");
-    console.log("server is running on port"+port);    
+
+//404 response 
+function send404Request(res){
+    res.writeHead(404,{'Context-Type':"text/plain"});
+    res.write("Error in server");
     res.end();
 };
 
+
+// handle the user request
+function onRequest(req,res){
+    if(req.method == 'GET' && req.url == '/'){
+    	res.writeHead(200,{'Context-Type':"text/html"});
+    	fs.createReadStream("./public/index.html").pipe(res);   // pipe() use to like pip it out like any file 
+    }
+    else{
+    	send404Request(res);
+    }
+};
+
 http.createServer(onRequest).listen(port);
-console.log("server is running on port"+port);  
+ 
